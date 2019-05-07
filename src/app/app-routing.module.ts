150de89ee519bad00d19ea465451afca5542b23c
Aman Raj <aman.raj@ivanti.com>
2019-05-07 15:11:29 +0530

    router changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactusComponent } from './contactus/contactus.component';
 import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
 {path: 'contact', component: ContactusComponent},
 {path: '', component: MainComponent},
+{path: 'about', component: AboutComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { LatestnewsComponent } from './latestnews/latestnews.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
@@ -27,6 +28,7 @@ import { MainComponent } from './main/main.component';
     LatestnewsComponent,
     ContactusComponent,
     MainComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
