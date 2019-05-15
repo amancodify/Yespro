4007b3159775efed6110809f747e39231fd16073
Aman Raj <aman.raj@ivanti.com>
2019-05-15 17:22:46 +0530

    env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCx4b0W7zGnwcaqG0tdB3etD0AQF73LXVA',
+    authDomain: 'yespro-c7097.firebaseapp.com',
+    databaseURL: 'https://yespro-c7097.firebaseio.com',
+    projectId: 'yespro-c7097',
+    storageBucket: 'yespro-c7097.appspot.com',
+    messagingSenderId: '321445192921',
+    appId: "1:321445192921:web:664c8161ebcaa5fd"
+  }
 };
