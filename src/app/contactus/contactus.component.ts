d45c1bfd87b9a2c13c4e4ab32507aaaf8133d7f2
Aman Raj <amandocx@gmail.com>
2019-05-15 23:56:56 +0530

    firebase data insert

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Item { 
+  fname: string; 
+  lname: string;
+  email: string;
+  message: string;
+}
 
 @Component({
   selector: 'app-contactus',
@@ -6,10 +15,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contactus.component.css']
 })
 export class ContactusComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
+  constructor(private user_queries: AngularFirestore) { 
+    
   }
-
+  ngOnInit() {
+    this.itemsCollection = this.user_queries.collection<Item>('user_queries')
+  }
+  addItem(item: Item) {
+    this.itemsCollection.add(item);
+  }
 }
